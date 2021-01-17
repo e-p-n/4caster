f592cd54ef5d73b32c26df7f3c4a4c430781b676
Eric Normann <eric.n@me.com>
2021-01-17 18:22:24 -0500

    Add list of recent searches

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,29 +1,55 @@
 var cityEl = document.querySelector("#city");
 var searchBtnEl = document.querySelector("#search");
 var weatherDisplayEl = document.querySelector("main");
-var tempScale, windScale, lat, lon, weatherCardEl, weatherCardBodyEl, cityName, forecastHolderEl;
+var cityListEl = document.querySelector(".cities");
+var tempScale, windScale, unit, lat, lon, weatherCardEl, weatherCardBodyEl, cityName, forecastHolderEl;
+var searchHistory = JSON.parse(localStorage.getItem("cities"));
+if (!searchHistory) {
+    searchHistory = [];
+}
 
 
-function searchWeather() {
-    let unitValues = document.getElementsByName("unit");
-    let unit;
-    cityName = cityEl.value;
 
-    for (var i = 0; i < unitValues.length; i++){
-
-        if(unitValues[i].checked) {
-            unit = unitValues[i].value;
-            if (unit === "metric") {
-                tempScale = "˚C";
-                windScale = " KM/H";
-            } else {
-                tempScale = "˚F";
-                windScale = " MPH"
-            }
+function loadCities() {
+    cityListEl.textContent="";
+    for(var i = searchHistory.length-1; i >=0; i--){
+        let cityListItem = searchHistory[i].city;
+        let cityListItemEl = document.createElement("a");
+        cityListItemEl.setAttribute("href", "#");
+        cityListItemEl.textContent= cityListItem;
+        cityListEl.appendChild(cityListItemEl);
+        cityListItemEl.addEventListener("click", function(){
+            searchWeather(cityListItem);
+        });
+        
+    }
+}
+function saveCity(newCity) {
+    for (var i = 0; i < searchHistory.length; i++) {
+        if (newCity == searchHistory[i].city) {
+            searchHistory.splice(i, 1);
         }
     }
+    let savedCity = {city: newCity};
+    searchHistory.push(savedCity);
+    localStorage.setItem("cities", JSON.stringify(searchHistory));
+    loadCities();
 
-    
+}
+
+function searchWeather(theCity) {
+    cityName = theCity;
+
+    let unitValue = document.getElementById("unit").checked;
+    if(unitValue) {
+        tempScale = "˚F";
+        windScale = " MPH";
+        unit="imperial";
+    } else {
+        tempScale = "˚C";
+        windScale = " KM/H";
+        unit="metric";
+    }
   
     if (cityName) {
         let coordAPICall = "http://api.openweathermap.org/geo/1.0/direct?q="+cityName+"&limit=1&appid=df74799927b7fd8a9f5fb2e7b85418e6";
@@ -39,6 +65,7 @@ function searchWeather() {
                 } else {
                     cityName = data[0].name;
                 }
+                saveCity(cityName);
                 cityEl.value = "";
                 let forecastAPICall = "http://api.openweathermap.org/data/2.5/onecall?lat="+lat+"&lon="+lon+"&exclude=minutely,hourly,alerts&units="+unit+"&appid=df74799927b7fd8a9f5fb2e7b85418e6";
 
@@ -67,6 +94,7 @@ function searchWeather() {
         alert("Fill out the damned form!")
     }
 }
+
 function buildWeatherCard(bgColor, isForecast, isFirst){
     
     weatherCardEl = document.createElement("div");
@@ -212,4 +240,5 @@ function weatherForecast(forecast, dayNo){
 
 }
 
-searchBtnEl.addEventListener("click", searchWeather);
+searchBtnEl.addEventListener("click", function() { searchWeather(cityEl.value) });
+loadCities();
